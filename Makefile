d7db7a7cea1f270d85345714697a0a479f8d02b7
Axetroy <axetroy.dev@gmail.com>
2023-05-11 20:13:14 +0800

    chore: update

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 .DEFAULT:
 build:
-	@wasm-pack build
+	@wasm-pack build --release --target web --out-dir out/web
+	@wasm-pack build --release --target nodejs --out-dir out/nodejs
+	@wasm-pack build --release --target no-modules --out-dir out/no-modules
+	@wasm-pack build --release --target bundler --out-dir out/bundler
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
+use digest::Digest;
 use js_sys::{Function, Uint8Array};
-use wasm_bindgen::prelude::*;
 use md5::Md5;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::Blob;
 use sha1::Sha1;
 use sha2::{Sha224, Sha256, Sha384, Sha512};
 use sha3::{Sha3_224, Sha3_256, Sha3_384, Sha3_512};
 use sm3::Sm3;
-use digest::Digest;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::Blob;
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub async fn md5(blob: Blob, cb: Function) -> Result<String, JsValue> {
@@ -93,17 +101,15 @@ pub async fn computed<D: Digest>(blob: Blob, cb: Function, hasher: &mut D) {
     let mut start: f64 = 0.0;
     for _ in 0..chunks {
         let mut end = start + step;
-        end = if end >= size {
-            size
-        } else {
-            end
-        };
-        let data = blob.slice_with_f64_and_f64(start, end).expect("slice blob failed!");
-        let buffer: JsValue = JsFuture::from(data.array_buffer()).await.expect("get arrayBuffer failed!");
+        end = if end >= size { size } else { end };
+        let data = blob
+            .slice_with_f64_and_f64(start, end)
+            .expect("slice blob failed!");
+        let buffer: JsValue = JsFuture::from(data.array_buffer())
+            .await
+            .expect("get arrayBuffer failed!");
         hasher.update(&Uint8Array::new(&buffer).to_vec());
         let _ = cb.call1(&JsValue::null(), &JsValue::from(start / size * 100.0));
         start += step;
-    };
+    }
 }
-
-
